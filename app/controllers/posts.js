e780220097baca0612c7bf177c76be61c2ea4749
Liam McLennan <liam.mclennan@gmail.com>
2012-05-27 21:18:16 +1000

    refactoring

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,16 +1,32 @@
 var request = require('request'),
   _ = require('underscore');
 
+// mock request
+request = function(options, callback) {
+  callback(null, { statusCode: 200 }, {
+    gists: [
+      {
+        repo: 1,
+        description: 'blah title',
+        files: ['blog_blah.md'],
+        created_at: new Date().toString()
+      },
+      {
+        repo: 2,
+        description: 'foo title',
+        files: ['blog_foo.md'],
+        created_at: new Date().toString()
+      }
+    ]
+  })
+};
+
 var Posts = function () {
   this.respondsWith = ['html', 'json', 'xml', 'js', 'txt'];
 
   this.index = function (req, resp, params) {
-    var gistListUrl = 'http://gist.github.com/api/v1/json/gists/liammclennan',
-      app = this;
-
-    request({ url: gistListUrl, json:true }, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        app.respond({
+    getGists(function (app, body) {
+      app.respond({
           gists: _.chain(body.gists)
             .filter(function (gist) {
               return /blog_.+\.md/.test(gist.files[0]);
@@ -27,11 +43,21 @@ var Posts = function () {
             .sortBy(function (gist) {
               return gist.created_at;
             })
-            .value()
-        });
+            .value().reverse()
+        });      
+    }, function () {}, this);
+  };
+
+  function getGists(successCallback, errorCallback, app) {
+    var gistListUrl = 'http://gist.github.com/api/v1/json/gists/liammclennan';
+    request({ url: gistListUrl, json: true }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        successCallback(app, body);
+      } else {
+        errorCallback();
       }
     });
-  };
+  }
 
   this.add = function (req, resp, params) {
     this.respond({params: params});
