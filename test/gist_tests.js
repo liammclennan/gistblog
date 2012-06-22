f22f8a31e0f95063a90c0bd78f21278c2b341fb1
Liam McLennan <liam.mclennan@gmail.com>
2012-06-22 20:20:50 +1000

    added tests and package.json

---
+++ test/gist_tests.js
@@ -0,0 +1,11 @@
+var request = require('request');
+
+describe('gist', function () {
+    describe('read gist', function () {
+        it('should do stuff', function () {
+            
+            
+            
+        });
+    });
+})
