a6f81ff2f80a3ca55c4c366ca81c4d61d900f37a
Adam <adamchester@gmail.com>
2012-07-17 09:44:18 +1000

    gist html content from Github API

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -97,23 +97,35 @@ var Posts = function () {
           .value();
 
         console.log('getting raw markdown for each blog gist');
-        async.parallel(asyncTasks, function onGotRawMarkdown(err, items) {
+        async.parallel(asyncTasks, function onGotRawMarkdown(err, rawMarkdownItems) {
           if (err) {
-            console.log("LOG: failed to get raw markdown, using cache. error = %j, items = %j", err);
-            successCallback(app, cache);
+            console.log("LOG: failed to get raw markdown, using cache. error = %j", err);
+            return successCallback(app, cache);
           }
-          else {
 
-            // put the markdown back into the correct items (the cached copy)
+          var convertMarkdownAsyncTasks = _.map(rawMarkdownItems, function (rawMarkdown) {
+            return async.apply(markdown.convertViaGithub, rawMarkdown);
+          });
+
+          // use GitHub to convert the markdown to HTML
+          console.log('converting raw markdown to html');
+          async.parallel(convertMarkdownAsyncTasks, function (err, htmlItems) {
+            if (err) {
+              console.log("LOG: failed to convert markdown via github. error = %s", err);
+              return successCallback(app, cache);
+            }
+
+            // put the html back into the correct object (the cached copy)
             // since async will give us the items in the correct order, we can
-            // can use the index to put the HTML content back into the cached item
-            _.chain(items).toArray().each(function (content_md, index) {
-              cacheBlogItemArray[index].content_html = markdown.convert(content_md);
+            // can use the arrat index.
+            _(htmlItems).each(function (content_html, index) {
+              cacheBlogItemArray[index].content_html = content_html;
             });
 
             lastGet = new Date();
             successCallback(app, body);
-          }
+          }); 
+
         });
       } else {
         console.log("LOG: failed to get gists from github. Using cache.");
